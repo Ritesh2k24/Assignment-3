d77eef459e60642c16994c25f7add4d5d7e937eb
Ritesh2k24 <162950856+Ritesh2k24@users.noreply.github.com>
2025-03-14 20:37:45 +0530

    Update migrate_to_aws.sh

--- migrate_to_aws.sh
+++ migrate_to_aws.sh
@@ -1,27 +1,33 @@
 #!/bin/bash
 
-AWS_INSTANCE_ID=$(aws ec2 run-instances \
-    --image-id ami-12345678 \
-    --count 1 \
-    --instance-type t2.micro \
-    --key-name MyKeyPair \
-    --security-groups my-security-group \
+AWS_INSTANCE_TYPE="t2.micro"
+AWS_AMI_ID="ami-12345678"
+AWS_KEY_NAME="MyKeyPair"
+AWS_SECURITY_GROUP="my-security-group"
+
+# Launch EC2 instance
+INSTANCE_ID=$(aws ec2 run-instances \
+    --image-id $AWS_AMI_ID \
+    --instance-type $AWS_INSTANCE_TYPE \
+    --key-name $AWS_KEY_NAME \
+    --security-groups $AWS_SECURITY_GROUP \
     --query 'Instances[0].InstanceId' --output text)
 
-echo "Instance $AWS_INSTANCE_ID created."
+# Wait for instance to be ready
+aws ec2 wait instance-running --instance-ids $INSTANCE_ID
 
-# Deploy the application to the new EC2 instance
-scp -i MyKeyPair.pem app.py ubuntu@$(aws ec2 describe-instances \
-    --instance-ids $AWS_INSTANCE_ID \
-    --query 'Reservations[0].Instances[0].PublicDnsName' \
-    --output text):/home/ubuntu/
+# Get instance public DNS
+INSTANCE_DNS=$(aws ec2 describe-instances \
+    --instance-ids $INSTANCE_ID \
+    --query 'Reservations[0].Instances[0].PublicDnsName' --output text)
 
-ssh -i MyKeyPair.pem ubuntu@$(aws ec2 describe-instances \
-    --instance-ids $AWS_INSTANCE_ID \
-    --query 'Reservations[0].Instances[0].PublicDnsName' \
-    --output text) << EOF
+# Deploy the application
+scp -i "$AWS_KEY_NAME.pem" app.py ubuntu@$INSTANCE_DNS:/home/ubuntu/
+ssh -i "$AWS_KEY_NAME.pem" ubuntu@$INSTANCE_DNS << EOF
 sudo apt update
 sudo apt install -y python3-pip
 pip3 install flask
-nohup python3 /home/ubuntu/app.py &
+nohup python3 /home/ubuntu/app.py > /dev/null 2>&1 &
 EOF
+
+echo "App migrated to EC2 at http://$INSTANCE_DNS:5000"
